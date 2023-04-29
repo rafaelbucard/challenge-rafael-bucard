b38da0609c735f98782a6d616b21483813176471
Rafael Pereira Buçard <rafael.bucard@landapp.com.br>
2023-04-29 15:50:20 -0300

    feat: primeiro commit   na main, criando o projeto e  base do input e o controller

---
+++ app/Http/Controllers/Calendar/DateController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Calendar;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DateController extends Controller
+{
+    public function dayNumber(Request $request)
+    {
+       
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Day Number</title>
+    <style>
+        /* Estilos CSS aqui */
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+        }
+        
+        .card {
+            background-color: #ffffff;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            max-width: 500px;
+            margin: auto;
+            padding: 20px;
+            border-radius: 10px;
+        }
+        
+        h1 {
+            text-align: center;
+            font-size: 2rem;
+            margin-bottom: 20px;
+        }
+        
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        
+        label {
+            font-size: 1.2rem;
+            margin-bottom: 10px;
+        }
+        
+        input[type="date"] {
+            font-size: 1.2rem;
+            padding: 10px;
+            border-radius: 5px;
+            border: none;
+            box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+            margin-bottom: 20px;
+            width: 100%;
+            max-width: 300px;
+        }
+        
+        button[type="submit"] {
+            background-color: #4CAF50;
+            color: white;
+            font-size: 1.2rem;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+        
+        button[type="submit"]:hover {
+            background-color: #3e8e41;
+        }
+    </style>
+</head>
+<body>
+    <div class="card">
+        <h1>Day Number</h1>
+        
+        <form method="POST" action="{{ route('day-number') }}">
+            @csrf
+            <label for="date">Data:</label>
+            <input type="date" id="date" name="date">
+            <br>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</body>
+</html>
